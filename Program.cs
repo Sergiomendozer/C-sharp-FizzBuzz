966b376c1e34edff21beaa25b04d4f10ae926de9
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-14 15:07:36 -0700

    function test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void test(){
+    Console.WriteLine("function called");
+}
+// Prompt use to enter an int
+Console.WriteLine("Enter a number to FizzBuzz from 0 to:");
+//! if a string alert user to enter int only or negative
+//coverts string into in
+int n = Convert.ToInt32(Console.ReadLine());
+
+//!testing 
+Console.WriteLine("test: " + n);
+test();
+// for (int i = 0; i < n; i++) 
+// {
+//   Console.WriteLine(i);
+// }
+// if (n== 20){
+//     Console.WriteLine("FB: " + n);
+// }
+
