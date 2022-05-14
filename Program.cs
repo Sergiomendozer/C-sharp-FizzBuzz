0575184dda8f156fc15870605ff6b1b5bf6db3b5
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-14 15:19:53 -0700

    fizzbuzz version one fuinished

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-void test(){
-    Console.WriteLine("function called");
-}
+// void test(){
+//     Console.WriteLine("function called");
+// }
 // Prompt use to enter an int
 Console.WriteLine("Enter a number to FizzBuzz from 0 to:");
 //! if a string alert user to enter int only or negative
@@ -10,12 +10,22 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 //!testing 
 Console.WriteLine("test: " + n);
-test();
-// for (int i = 0; i < n; i++) 
-// {
-//   Console.WriteLine(i);
-// }
-// if (n== 20){
-//     Console.WriteLine("FB: " + n);
-// }
+// test();
+for (int i = 0; i < n + 1; i++) 
+{
+    if (i == 0){
+    Console.WriteLine(i);
+    } 
+    else if (i%5 == 0 && i%3 == 0 ){
+    Console.WriteLine(i+" FizzBuzz");
+    }
+    else if (i%3 == 0){
+    Console.WriteLine(i+" Fizz");
+    } 
+    else if (i%5 == 0){
+    Console.WriteLine(i+" Buzz");
+    }
+
+  else{Console.WriteLine(i);}
+}
 
