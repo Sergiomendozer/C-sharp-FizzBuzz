e3bd8da164e739551e59507024cb98c042aa2865
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-14 16:40:53 -0700

    fizzbuzz function made

--- Program.cs
+++ Program.cs
@@ -1,31 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// void test(){
-//     Console.WriteLine("function called");
+// void checker_for_negative(n){
+//     Console.WriteLine("function called"+ n);
 // }
+
+static int FizzBuzz(int n){
+    for (int i = 0; i < n + 1; i++){
+        if (i == 0){
+        Console.WriteLine(i);
+        }
+        else if (i%5 == 0 && i%3 == 0 ){
+        Console.WriteLine("FizzBuzz");
+        }
+        else if (i%3 == 0){
+        Console.WriteLine("Fizz");
+        } 
+        else if (i%5 == 0){
+        Console.WriteLine("Buzz");
+        }
+        else{
+        Console.WriteLine(i);
+    }
+    }
+    return (n);}
 // Prompt use to enter an int
-Console.WriteLine("Enter a number to FizzBuzz from 0 to:");
+//? Console.WriteLine("Enter a number to FizzBuzz from 0 to:");
 //! if a string alert user to enter int only or negative
-//coverts string into in
-int n = Convert.ToInt32(Console.ReadLine());
+//coverts string into integer
+//? int n = Convert.ToInt32(Console.ReadLine());
+int n = 27;
+// checker_for_negative(n);
+// //!testing 
+// Console.WriteLine("test: " + n);
+FizzBuzz(n);
 
-//!testing 
-Console.WriteLine("test: " + n);
-// test();
-for (int i = 0; i < n + 1; i++) 
-{
-    if (i == 0){
-    Console.WriteLine(i);
-    } 
-    else if (i%5 == 0 && i%3 == 0 ){
-    Console.WriteLine(i+" FizzBuzz");
-    }
-    else if (i%3 == 0){
-    Console.WriteLine(i+" Fizz");
-    } 
-    else if (i%5 == 0){
-    Console.WriteLine(i+" Buzz");
-    }
 
-  else{Console.WriteLine(i);}
-}
+
+
+
+
+
+
+
+
+
+
+// for (int i = 0; i < n + 1; i++) 
+// {
+//     if (i == 0){
+//     Console.WriteLine(i);
+//     } 
+//     else if (i%5 == 0 && i%3 == 0 ){
+//     Console.WriteLine(i+" FizzBuzz");
+//     }
+//     else if (i%3 == 0){
+//     Console.WriteLine(i+" Fizz");
+//     } 
+//     else if (i%5 == 0){
+//     Console.WriteLine(i+" Buzz");
+//     }
+//     else{
+//         Console.WriteLine(i);
+//     }
+// }
 
